992034bdd36c8cf370b419967074f1cd44c7d2d5
lolylynm <lolylynm@gmail.com>
2021-08-06 11:40:52 +0200

    all elements on the side bar have headers

--- pages/views.py
+++ pages/views.py
@@ -35,6 +35,74 @@ def profilecovid19t(request):
     context={}
     return render(request, 'brti_covid_19_weekly_statistics_tool/general.html', context)
 
+def vleid(request):
+    context={}
+    return render(request, 'brti_vl_eid_weekly_statistics_tool/specimens_run.html', context)
+
+def vleid2(request):
+    context={}
+    return render(request, 'brti_vl_eid_weekly_statistics_tool/specimens_received.html', context)
+
+def vleid3(request):
+    context={}
+    return render(request, 'brti_vl_eid_weekly_statistics_tool/reasons_for_failure.html', context)
+
+def labcovidweekly(request):
+    context={}
+    return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/top.html', context)
+
+def labcovidweekly2(request):
+    context={}
+    return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/Specimensrun.html', context)
+
+def labcovidweekly3(request):
+    context={}
+    return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/specimenreceived.html', context)
+
+def labcovidweekly4(request):
+    context={}
+    return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/Reasonsforfailures.html', context)
+
+def labcovidweekly5(request):
+    context={}
+    return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/electricoutagestool.html', context)
+
+def labeidweekly(request):
+    context={}
+    return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/top.html', context)
+
+def labeidweekly2(request):
+    context={}
+    return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/Specimens_Run.html', context)
+
+def labeidweekly3(request):
+    context={}
+    return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/Specimens_Received.html', context)
+
+def labeidweekly4(request):
+    context={}
+    return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/general.html', context)
+
+def labbrtiweekly(request):
+    context={}
+    return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/top.html', context)
+
+def labbrtiweekly2(request):
+    context={}
+    return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/Specimensrun.html', context)
+
+def labbrtiweekly3(request):
+    context={}
+    return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/specimenreceived.html', context)
+
+def labbrtiweekly4(request):
+    context={}
+    return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/Reasonsforfailures.html', context)
+
+def labbrtiweekly5(request):
+    context={}
+    return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/electricoutagestool.html', context)
+
 
 
 
