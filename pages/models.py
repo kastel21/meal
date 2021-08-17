0e087da84d04115af8e90ec158b7fcae50af96e7
kastel21 <kastel21@outlook.com>
2021-08-17 19:45:45 +0200

    all 3 masvingo models done-ish

--- pages/models.py
+++ pages/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 
 # Create your models here.
 
-
+#skip masvingo brti covid weekly
 
 
 class Record(models.Model):
@@ -36,8 +36,8 @@ class Record(models.Model):
         return i
 
 
-
-class Specimens_recieved(models.Model):
+# masving_brti_vl_weekly_statistics_tool_31-6_june_2021
+class Specimens_recieved_brti_vl_weekly(models.Model):
     day_of_week = models.CharField(max_length=20,null=False)
 
 
@@ -167,7 +167,7 @@ class Specimens_recieved(models.Model):
 
 
 
-class Specimens_run(models.Model):
+class Specimens_run_brti_vl_weekly(models.Model):
     day_of_week = models.CharField(max_length=20,null=False)
 
 
@@ -260,7 +260,7 @@ class Specimens_run(models.Model):
 
 
 
-class Electric_outage(models.Model):
+class Electric_outage_brti_vl_weekly(models.Model):
     day_of_week = models.CharField(max_length=20,null=False)
 
 
@@ -280,7 +280,7 @@ class Electric_outage(models.Model):
 
 
 
-class Reasons_for_failure(models.Model):
+class Reasons_for_failure_brti_vl_weekly(models.Model):
     day_of_week = models.CharField(max_length=20,null=False)
 #roche
     roche_plasma_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
@@ -290,7 +290,7 @@ class Reasons_for_failure(models.Model):
     roche_plasma_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
     roche_plasma_number_of_failed_tests_due_to_processing_error = models.IntegerField()
     roche_plasma_number_of_failed_tests_due_to_other = models.IntegerField()
-    dqa_check_plasma = models.IntegerField()
+    roche_dqa_check_plasma = models.IntegerField()
 
     roche_dbs_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
     roche_dbs_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
@@ -368,10 +368,10 @@ class Reasons_for_failure(models.Model):
 
 
 
-#General Info
+#General Info masvingo brti_covid_19_weekly_statistics_tool
 
 
-class General_info(models.Model):
+class General_info_covid_19(models.Model):
     general_comments_regarding_testing_and_challenges_faced_by_the_laboratory = models.CharField( max_length=5000, null=True)
     number_of_staff_who_tested_positive_to_covid_19_at_vl_lab	= models.IntegerField()
     number_of_staff_who_have_been_vaccinated	= models.IntegerField()
@@ -381,7 +381,14 @@ class General_info(models.Model):
 
 
 
-class Specimens_received_covid19(models.Model):
+
+
+
+
+#another 
+
+
+class Specimens_received_covid_19(models.Model):
     samples_carried_over_previous_weeks	= models.IntegerField()
 
     samples_received_current_week_nasopharyngeal_swab = models.IntegerField()
@@ -404,34 +411,34 @@ class Specimens_received_covid19(models.Model):
     total_results_dispatched_by_lab	=	models.IntegerField()
     comment= models.CharField( max_length=5000, null=True)
 
-class Machine_downtime_Reagent_stock_out_tool_covid19(models.Model):
-    machine_breakdown_number_abbott	=	models.IntegerField()
-    machine_breakdown_number_bmx	=	models.IntegerField()
-    machine_breakdown_number_genexpert	=	models.IntegerField()
-    machine_breakdown_number_quant_studio_3	=	models.IntegerField()
-    machine_breakdown_number_hologic_panther	=	models.IntegerField()
-    machine_breakdown_number_comments	= models.CharField( max_length=5000, null=True)
+# class Machine_downtime_Reagent_stock_out_tool_covid_19(models.Model):
+#     machine_breakdown_number_abbott	=	models.IntegerField()
+#     machine_breakdown_number_bmx	=	models.IntegerField()
+#     machine_breakdown_number_genexpert	=	models.IntegerField()
+#     machine_breakdown_number_quant_studio_3	=	models.IntegerField()
+#     machine_breakdown_number_hologic_panther	=	models.IntegerField()
+#     machine_breakdown_number_comments	= models.CharField( max_length=5000, null=True)
 
-    machine_downtime_days_abbott = 	models.IntegerField()
-    machine_downtime_days_bmx = 	models.IntegerField()
-    machine_downtime_days_genexpert = 	models.IntegerField()
-    machine_downtime_days_quant_studio_3 = 	models.IntegerField()
-    machine_downtime_days_hologic_panther = 	models.IntegerField()
-    machine_downtime_days_comments = 	models.CharField( max_length=5000, null=True)
+#     machine_downtime_days_abbott = 	models.IntegerField()
+#     machine_downtime_days_bmx = 	models.IntegerField()
+#     machine_downtime_days_genexpert = 	models.IntegerField()
+#     machine_downtime_days_quant_studio_3 = 	models.IntegerField()
+#     machine_downtime_days_hologic_panther = 	models.IntegerField()
+#     machine_downtime_days_comments = 	models.CharField( max_length=5000, null=True)
 
-    #reagent_stockout	
+#     #reagent_stockout	
 
-    reagent_stockout_abbott = 	models.IntegerField()
-    reagent_stockout_bmx = 	models.IntegerField()
-    reagent_stockout_genexpert = 	models.IntegerField()
-    reagent_stockout_quant_studio_3 = 	models.IntegerField()
-    reagent_stockout_hologic_panther = 	models.IntegerField()
-    reagent_stockout_comments = 	models.CharField( max_length=5000, null=True)
-
-
+#     reagent_stockout_abbott = 	models.IntegerField()
+#     reagent_stockout_bmx = 	models.IntegerField()
+#     reagent_stockout_genexpert = 	models.IntegerField()
+#     reagent_stockout_quant_studio_3 = 	models.IntegerField()
+#     reagent_stockout_hologic_panther = 	models.IntegerField()
+#     reagent_stockout_comments = 	models.CharField( max_length=5000, null=True)
 
 
-class Specimens_run_covid19(models.Model):
+
+
+class Specimens_run_covid_19(models.Model):
     #Roche
     tests_done_abbott_run = models.IntegerField()
     tests_done_abbott_failed_but_eligibale_for_repeat= models.IntegerField()
@@ -488,9 +495,9 @@ class Specimens_run_covid19(models.Model):
 
 
 
-#masving_brti_vl_weekly_statistics_tool_31-6_june_2021
+#masving_brti_weekly_statistics_tool_31-6_june_2021
 
-class Specimens_run(models.Model):
+class Specimens_run_brti_weekly(models.Model):
     day_of_week = models.CharField(max_length=20,null=False)
 
 
@@ -580,3 +587,269 @@ class Specimens_run(models.Model):
 
     total_ncs_from_audit=models.TextField(max_length=5000,null=True)
     ncs_not_yet_closed=models.TextField(max_length=5000,null=True)
+
+
+
+class Specimens_received_brti_weekly(models.Model):
+    date_of_week= models.IntegerField()
+
+    samples_carried_over_previous_weeks_never_tested_plasma = models.IntegerField()
+    samples_carried_over_previous_weeks_never_tested_dbs = models.IntegerField()
+
+    samples_carried_over_previous_failed_samples_plasma = models.IntegerField()
+    samples_carried_over_previous_failed_samples_dbs = models.IntegerField()
+
+
+
+    samples_received_current_week_plasma = models.IntegerField()
+    samples_received_current_week_dbs = models.IntegerField()
+
+    samples_rejected_current_week_plasma = models.IntegerField()
+    samples_rejected_current_week_dbs = models.IntegerField()
+    
+    total_samples_received_current_week_plasma = models.IntegerField()
+    total_samples_received_current_week_dbs = models.IntegerField()
+
+    number_of_samples_entered_into_lims_on_day_of_arrival_plasma = models.IntegerField()
+    number_of_samples_entered_into_lims_on_day_of_arrival_dbs = models.IntegerField()
+
+    number_of_samples_entered_into_lims_after_day_of_arrival_plasma = models.IntegerField()
+    number_of_samples_entered_into_lims_after_day_of_arrival_dbs = models.IntegerField()
+
+    number_of_hours_lims_was_functional	 = models.IntegerField()
+
+    total_samples_current_plus_carryover_plasma= models.IntegerField()
+    total_samples_current_plus_carryover_dbs= models.IntegerField()
+
+    samples_reffered_plasma = models.IntegerField()
+    samples_reffered_dbs = models.IntegerField()
+
+    lab_samples_reffered_to = models.IntegerField()
+
+    percentage_rejection_rate_plasma = models.DecimalField(decimal_places=5,  max_digits=5)
+    percentage_rejection_rate_dbs = models.DecimalField(decimal_places=5,  max_digits=5)
+
+
+    number_of_results_printed_from_lims_plasma = models.IntegerField()
+    number_of_results_printed_from_lims_dbs = models.IntegerField()
+
+    total_results_dispatched_by_lab_plasma	= models.IntegerField()
+    total_results_dispatched_by_lab_dbs	= models.IntegerField()
+
+    total_results_dispatched_by_lab_via_sms_plasma	= models.IntegerField()
+    total_results_dispatched_by_lab_via_sms_dbs	= models.IntegerField()
+
+
+
+    reasons_for_rejections_sample_quality_compromised_plasma = models.IntegerField()
+    reasons_for_rejections_sample_quality_compromised_dbs = models.IntegerField()
+
+
+    reasons_for_rejections_sample_quality_compromised_sample_quality_insufficient_plasma = models.IntegerField()
+    reasons_for_rejections_sample_quality_compromised_sample_quality_insufficient_dbs = models.IntegerField()
+
+
+    reasons_for_rejections_sample_quality_compromised_sample_information_mismatch_plasma = models.IntegerField()
+    reasons_for_rejections_sample_quality_compromised_sample_information_mismatch_dbs = models.IntegerField()
+
+
+    reasons_for_rejections_sample_quality_compromised_sample_or_request_form_missing_plasma = models.IntegerField()
+    reasons_for_rejections_sample_quality_compromised_sample_or_request_form_missing_dbs = models.IntegerField()
+
+
+    reasons_for_rejections_sample_quality_compromised_qda_check_plasma = models.IntegerField()
+    reasons_for_rejections_sample_quality_compromised_qda_check_dbs = models.IntegerField()
+
+
+    reasons_for_sample_refferal_reagent_or_kit_stockout_plasma= models.IntegerField()
+    reasons_for_sample_refferal_reagent_or_kit_stockout_dbs= models.IntegerField()
+
+
+    reasons_for_sample_refferal_instrument_mechanical_failure_plasma= models.IntegerField()
+    reasons_for_sample_refferal_instrument_mechanical_failure_dbs= models.IntegerField()
+
+    reasons_for_sample_refferal_insufficient_instrument_capacity_plasma= models.IntegerField()
+    reasons_for_sample_refferal_insufficient_instrument_capacity_dbs= models.IntegerField()
+
+    reasons_for_sample_refferal_insufficient_hr_capacity_plasma= models.IntegerField()
+    reasons_for_sample_refferal_insufficient_hr_capacity_dbs= models.IntegerField()
+
+    reasons_for_sample_refferal_dqa_check_plasma= models.IntegerField()
+    reasons_for_sample_refferal_dqa_check_dbs= models.IntegerField()
+
+   
+   
+   
+   
+
+   
+   
+    comments=models.TextField("Please input any comments regarding sample carryover, samples received, samples rejected, rejection rate, TAT, machine breakdowns & down time, reagent stock out if applicable", max_length=5000, null=True)
+
+
+class Reasons_for_failure_brti_weekly(models.Model):
+    day_of_week = models.CharField(max_length=20,null=False)
+#roche
+    roche_plasma_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    roche_plasma_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    roche_plasma_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    roche_plasma_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    roche_plasma_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    roche_plasma_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    roche_plasma_number_of_failed_tests_due_to_other = models.IntegerField()
+    roche_dqa_check_plasma = models.IntegerField()
+
+    roche_dbs_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    roche_dbs_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    roche_dbs_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    roche_dbs_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    roche_dbs_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    roche_dbs_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    roche_dbs_number_of_failed_tests_due_to_other = models.IntegerField()
+    roche_dqa_check_dbs = models.IntegerField()
+
+#bmx
+
+    bmx_plasma_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    bmx_plasma_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    bmx_plasma_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    bmx_plasma_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    bmx_plasma_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    bmx_plasma_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    bmx_plasma_number_of_failed_tests_due_to_other = models.IntegerField()
+    bmx_dqa_check_plasma = models.IntegerField()
+
+    bmx_dbs_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    bmx_dbs_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    bmx_dbs_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    bmx_dbs_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    bmx_dbs_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    bmx_dbs_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    bmx_dbs_number_of_failed_tests_due_to_other = models.IntegerField()
+    bmx_dqa_check_dbs = models.IntegerField()
+
+
+
+#abbott
+
+    abbott_plasma_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    abbott_plasma_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    abbott_plasma_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    abbott_plasma_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    abbott_plasma_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    abbott_plasma_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    abbott_plasma_number_of_failed_tests_due_to_other = models.IntegerField()
+    abbott_dqa_check_plasma = models.IntegerField()
+
+    abbott_dbs_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    abbott_dbs_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    abbott_dbs_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    abbott_dbs_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    abbott_dbs_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    abbott_dbs_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    abbott_dbs_number_of_failed_tests_due_to_other = models.IntegerField()
+    abbott_dqa_check_dbs = models.IntegerField()
+
+
+
+
+#Hologic Panther
+
+    hologic_panther_plasma_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    hologic_panther_plasma_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    hologic_panther_plasma_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    hologic_panther_plasma_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    hologic_panther_plasma_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    hologic_panther_plasma_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    hologic_panther_plasma_number_of_failed_tests_due_to_other = models.IntegerField()
+    hologic_panther_dqa_check_plasma = models.IntegerField()
+
+    hologic_panther_dbs_number_of_failed_tests_due_to_sample_quality_issues = models.IntegerField()
+    hologic_panther_dbs_number_of_failed_tests_due_to_reagent_quality_issues = models.IntegerField()
+    hologic_panther_dbs_number_of_failed_tests_due_to_due_to_qc_failure = models.IntegerField()
+    hologic_panther_dbs_number_of_failed_tests_due_to_power_failure = models.IntegerField()
+    hologic_panther_dbs_number_of_failed_tests_due_to_mechanical_failure = models.IntegerField()
+    hologic_panther_dbs_number_of_failed_tests_due_to_processing_error = models.IntegerField()
+    hologic_panther_dbs_number_of_failed_tests_due_to_other = models.IntegerField()
+    hologic_panther_dqa_check_dbs = models.IntegerField()
+
+
+
+
+class Electric_outage_brti_vl_weekly(models.Model):
+    day_of_week = models.CharField(max_length=20,null=False)
+
+
+    number_of_hours_with_no_electricity_per_day	= models.IntegerField()
+    number_of_hours_generator_was_on_per_day	= models.IntegerField()
+    litres_of_fuel_added_to_generator_per_day	= models.IntegerField()
+    number_of_hours_machines_was_not_being_used_due_to_power_cut_per_day	= models.IntegerField()
+    total_tests_done_per_day_using_generator	= models.IntegerField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -16,91 +16,226 @@ def home(request):
     return render(request, 'base.html', context)
 
 def profile(request):
-    context={}
+    # Create your views here.
+
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
+
     return render(request, 'brti_covid_19_weekly_statistics_tool/general.html', context)
 
 def profilecovid19to(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+        form = ComplaintForm()
+        context = {'form': form}
     return render(request, 'brti_covid_19_weekly_statistics_tool/Specimens_Run.html', context)
 
 def profilecovid(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'brti_covid_19_weekly_statistics_tool/Specimens_Received.html', context)
 
 def profilecovid19(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'brti_covid_19_weekly_statistics_tool/Machine_Downtime_&_Reagent_stock_out_tool.html', context)
 
 def profilecovid19t(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'brti_covid_19_weekly_statistics_tool/general.html', context)
 
 def vleid(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'brti_vl_eid_weekly_statistics_tool/specimens_run.html', context)
 
 def vleid2(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'brti_vl_eid_weekly_statistics_tool/specimens_received.html', context)
 
 def vleid3(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'brti_vl_eid_weekly_statistics_tool/reasons_for_failure.html', context)
 
 def labcovidweekly(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/top.html', context)
 
 def labcovidweekly2(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/Specimensrun.html', context)
 
 def labcovidweekly3(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/specimenreceived.html', context)
 
 def labcovidweekly4(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/Reasonsforfailures.html', context)
 
 def labcovidweekly5(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masving_brti_vl_weekly_statistics_tool_31-6_june_2021/electricoutagestool.html', context)
 
 def labeidweekly(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/top.html', context)
 
 def labeidweekly2(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/Specimens_Run.html', context)
 
 def labeidweekly3(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/Specimens_Received.html', context)
 
 def labeidweekly4(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_covid_19_weekly_statistics_tool_31-6_June_2021/general.html', context)
 
 def labbrtiweekly(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/top.html', context)
 
 def labbrtiweekly2(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/Specimensrun.html', context)
 
 def labbrtiweekly3(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/specimenreceived.html', context)
 
 def labbrtiweekly4(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/Reasonsforfailures.html', context)
 
 def labbrtiweekly5(request):
-    context={}
+    if request.method == 'POST':
+        form = ComplaintForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'success.html')
+    form = ComplaintForm()
+    context = {'form': form}
     return render(request, 'masvingo_brti_weekly_statistics_tool_june_2021/electricoutagestool.html', context)
 
 
